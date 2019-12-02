dca1214787f29c5eed54da6aae907043adb1585e
ximing <ximing@meituan.com>
2019-12-02 21:30:41 +0800

    feat: 调整目录

---
+++ client/bin/pack.js
@@ -0,0 +1,33 @@
+const packager = require('electron-packager')
+const path = require('path')
+const pkg = require('../package')
+const flat = require('electron-osx-sign').flat
+
+const resourcesPath = path.join(__dirname, '..', 'resources')
+
+packager({
+  dir: path.join(__dirname, '..'),
+  appCopyright: '© 2019, ximing',
+  asar: true,
+  overwrite: true,
+  electronVersion: pkg.electronVersion,
+  icon: path.join(resourcesPath, 'icns', 'MyIcon'),
+  out: path.join(__dirname, '..', 'dist', 'out'),
+  platform: 'mas',
+  appBundleId: `ximing.${pkg.name}`,
+  appCategoryType: 'public.app-category.developer-tools',
+  osxSign: {
+    type: process.env.NODE_ENV === 'production' ? 'distribution' : 'development',
+    entitlements: path.join(resourcesPath, 'parent.plist'),
+    'entitlements-inherit': path.join(resourcesPath, 'child.plist')
+  }
+}).then((res) => {
+  const app = path.join(res[0], `${pkg.productName}.app`)
+  console.log('flating...', app)
+  flat({ app }, function done (err) {
+    if (err) {
+      throw err
+    }
+    process.exit(0);
+  })
+})
